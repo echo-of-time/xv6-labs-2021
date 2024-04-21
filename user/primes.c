e4a7f566b594cf2bf2dab0ae3ae66827b80b2fae
echo-of-time <1909880919@qq.com>
2024-04-21 11:21:42 +0800

    update primes.c

--- user/primes.c
+++ user/primes.c
@@ -2,6 +2,35 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+void primes(int fd){
+    // p = get a number from left neighbor
+    // print p
+    // loop:
+    //     n = get a number from left neighbor
+    //     if (p does not divide n)
+    //         send n to right neighbor
+
+    int pipeline[2];
+    pipe(pipeline);
+
+    int left_num;
+    read(fd, &left_num, sizeof(int));
+    close(fd);
+    printf("primes: %d\n", left_num);
+    int pid = fork();
+    if (pid == 0) {
+        primes(pipeline[1]);
+    } else if (pid > 0){
+        int n;
+        read(pipeline[0], &n, sizeof(int));
+        close(pipeline[0]);
+        if (n % left_num != 0) {
+            write(pipeline[1], &n, sizeof(int));
+        }
+        close(pipeline[1]);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 1)
@@ -17,31 +46,32 @@ int main(int argc, char *argv[])
     {
         write(pipeline[1], &i, sizeof(int));
     }
-
-    // p = get a number from left neighbor
-    // print p
-    // loop:
-    //     n = get a number from left neighbor
-    //     if (p does not divide n)
-    //         send n to right neighbor
+    close(pipeline[1]);
+    primes(pipeline[0]);
+    close(pipeline[0]);
 
     int p, n;
     read(pipeline[0], &p, sizeof(int));
     printf("prime: %d\n", p);
-    while (read(pipeline[0], &n, sizeof(int)) > 0) {
-        if (n % p != 0) {
+    while (read(pipeline[0], &n, sizeof(int)) > 0)
+    {
+        if (n % p != 0)
+        {
             write(pipeline[1], &n, sizeof(int));
             int pid = fork();
-            if (pid == 0) {
+            if (pid == 0)
+            {
                 // child
                 read(pipeline[0], &p, sizeof(int));
                 printf("prime: %d\n", p);
-            }else {
+            }
+            else
+            {
                 // parent
                 wait(0);
             }
         }
     }
-    
+
     exit(0);
 }
